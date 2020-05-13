3a37a394441f1bae7765259a791ca74056a6dc36
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-05-12 20:41:55 -0700

    init commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>To do</Text>
     </View>
   );
 }
@@ -12,7 +12,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#32a89e',
     alignItems: 'center',
     justifyContent: 'center',
   },
